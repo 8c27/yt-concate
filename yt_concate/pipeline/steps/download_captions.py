e7ad8475868100de4216e2ebd7b1874076682e26
Weicheng Lu <spicychicken0627@gmail.com>
2023-03-28 12:05:35 +0800

    Reformat code

--- yt_concate/pipeline/steps/download_captions.py
+++ yt_concate/pipeline/steps/download_captions.py
@@ -5,23 +5,7 @@ from pytube import YouTube
 from yt_concate.pipeline.steps.step import Step
 from yt_concate.pipeline.steps.step import StepException
 
-'''
-class DownloadCaptions(Step):
-    def process(self, data, inputs, utils):
-        start = time.time()
-        for url in data:
-            if utils.captions_file_exists(url):
-                continue
-            source = YouTube(url)
-            caption = source.captions.get_by_language_code('a.en')
-            caption_convert_to_srt = (caption.generate_srt_captions())
-            text_file = open(utils.get_caption_path(url), "w", encoding="utf-8")
-            text_file.write(caption_convert_to_srt)
-            text_file.close()
 
-        end = time.time()
-        print('took',end-start,'seconds')
-'''
 class DownloadCaptions(Step):
     def process(self, data, inputs, utils):
         start = time.time()
@@ -40,5 +24,4 @@ class DownloadCaptions(Step):
                 print("An Error for :", url)
                 continue
         end = time.time()
-        print('took', end-start, 'seconds')
-
+        print('took', end - start, 'seconds')
